f8fbf9961a8c21064d4c2d350b30075704704bb4
dengdaichao <dengdaichao@vandream.com>
2020-10-30 16:09:26 +0800

    first commit

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,46 @@
+<template>
+  <div class="hello">
+    <h1>{{ msg }}</h1>
+    <input
+      type="text"
+      v-model="msg"
+    >
+    <!-- <input
+      type="text"
+      :value="msg"
+      @input="msg = $event.target.value"
+    > -->
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HelloWorld',
+  data () {
+    return {
+      msg: ''
+    }
+  },
+  methods: {
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h1,
+h2 {
+  font-weight: normal;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
